cb63dae7621b57b7e3fc6c4308901d19f5c62ead
Abdelrahman Mohamed <54869237+abdelrahman298@users.noreply.github.com>
2023-12-25 20:02:07 +0200

    send the object to local sorage and display in the page

---
+++ js/script.js
@@ -0,0 +1,59 @@
+var siteNameInput = document.getElementById("siteName");
+var siteUrlInput = document.getElementById("siteUrl");
+var submitBtn = document.getElementById("submitBtn");
+var tableBody = document.getElementById("tableBody");
+
+var websites = [];
+if (localStorage.getItem("websitesList") != null) {
+  websites = JSON.parse(localStorage.getItem("websitesList"));
+  displayWebsites(websites);
+  // console.log(websites.push(JSON.parse(localStorage.getItem("websitesList"))));
+}
+
+function addWebsite() {
+  //?save website in object
+  var websiteData = {
+    siteName: siteNameInput.value,
+    siteUrl: siteUrlInput.value,
+  };
+  //?send it to array
+  websites.push(websiteData);
+  //?send it to local storage
+  localStorage.setItem("websitesList", JSON.stringify(websites));
+  //?display the array in the page
+  displayWebsites(websites);
+  //?clear the inputs
+  // clearInput();
+}
+
+function clearInput() {
+  siteNameInput.value = "";
+  siteUrlInput.value = "";
+}
+
+function displayWebsites(array) {
+  var cartoona = ``;
+
+  for (var i = 0; i < array.length; i++) {
+    var site = `
+      <tr>
+      <th scope="row">${i + 1}</th>
+      <td>${array[i].siteName}</td>
+      <td>
+      <a href="https://${array[i].siteUrl}"  target="_blank">
+      <button type="button" class="btn btn-visit">
+        <i class="fa-solid fa-eye"></i> Visit
+      </button>
+      </a>
+        
+      </td>
+      <td>
+        <button type="button" class="btn btn-danger">
+          <i class="fa-solid fa-trash-can"></i> Delete
+        </button>
+      </td>
+    </tr>`;
+    cartoona += site;
+  }
+  tableBody.innerHTML = cartoona;
+}
